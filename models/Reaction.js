676f25d0a07cadff442a3f6cbbc15fb40728a897
gato365 <immanuelwilliams@gato365.com>
2023-02-11 00:01:47 -0800

    Created reaction for model and index file

---
+++ models/Reaction.js
@@ -0,0 +1,40 @@
+// Get Schema and Types from Mongoose
+const { Schema, Types } = require('mongoose');
+
+// Create a reactionSchema
+const reactionSchema = new Schema(
+    // Define the fields for the reactionSchema
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 280
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (createdAtVal) => dateFormat(createdAtVal)
+        }
+
+    },
+    {
+        // Use getters and virtuals when data is requested
+        toJSON: {
+            getters: true
+        },
+        // Prevent virtuals from creating duplicate of _id as `id`
+        id: false
+    }
+
+);
+
+// Export the reactionSchema
+module.exports = reactionSchema;
