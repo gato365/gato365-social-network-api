5cd0ac9537c7793077ed1939980a6e35422d0176
gato365 <immanuelwilliams@gato365.com>
2023-02-10 23:46:31 -0800

    Develop the thoughts model

---
+++ models/Thought.js
@@ -0,0 +1,35 @@
+// Declare Schema and Types from Mongoose
+const { Schema, Types } = require('mongoose');
+
+
+const thoughtSchema = new Schema(
+    // Define the fields for the Thought model
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (createdAtVal) => dateFormat(createdAtVal),
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        reactions: [reactionSchema],
+    },
+    {
+        // Use getters and virtuals when data is requested
+        toJSON: {
+            virtuals: true,
+            getters: true,
+        },
+        // Prevent virtuals from creating duplicate of _id as `id`
+        id: false,
+    });
+
+module.exports = thoughtSchema;
