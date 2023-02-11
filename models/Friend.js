db2c2f6cf17f624f708bccf695df889d418db9ad
gato365 <immanuelwilliams@gato365.com>
2023-02-11 00:19:58 -0800

    Got rid of friends model file, do not need it

--- models/Friend.js
+++
@@ -1,8 +0,0 @@
-// Get Schema and Types from Mongoose
-const { Schema, Types } = require('mongoose');
-
-// Create a friendSchema
-const friendSchema = new Schema();
-
-// Export the friendSchema
-module.exports = friendSchema;
