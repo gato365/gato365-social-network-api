c03d681c30f53597f66f4133151a80c0e1105c38
gato365 <immanuelwilliams@gato365.com>
2023-02-11 00:32:28 -0800

    Made/Created Thoughts Controller

---
+++ controllers/thoughtController.js
@@ -0,0 +1,22 @@
+// Get Thoughts and User Models
+const { Thought, User } = require('../models');
+
+
+
+module.exports = {
+    // Post Thought
+    createThought(req, res) {
+        Thought.create(req.body)
+            .then((thought) => res.json(thought))
+            .catch((err) => {
+                console.log(err);
+                res.status(500).json(err);
+            });
+    },
+    // Get All Thoughts
+    getThoughts(req, res) {
+        Thought.find()
+            .then((thoughts) => res.json(thoughts))
+            .catch((err) => res.status(500).json(err));
+    }
+};
