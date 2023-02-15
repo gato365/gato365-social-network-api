165b29922c61be046abfcb366d28bc24490fdde1
gato365 <immanuelwilliams@gato365.com>
2023-02-15 06:45:21 -0800

    Made the capacity to delete thought

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -18,6 +18,18 @@ module.exports = {
         Thought.find()
             .then((thoughts) => res.json(thoughts))
             .catch((err) => res.status(500).json(err));
+    },
+    //Delete Thought
+    deleteThought(req, res) {
+        Thought.findOneAndDelete({ _id: req.params.thoughtId })
+            .then((thought) => {
+                if (!thought) {
+                    res.status(404).json({ message: 'No thought found with this id!' });
+                    return;
+                }
+                res.json(thought);
+            })
+            .catch((err) => res.status(500).json(err));
     }
 
 
--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -3,11 +3,14 @@ const router = require('express').Router();
 const {
     createThought,
     getAllThoughts,
+    deleteThought,
 } = require('../../controllers/thoughtController');
 
 // /api/thoughts
 router.route('/').get(getAllThoughts).post(createThought);
 
+// /api/thoughts/:thoughtId
+router.route('/:thoughtId').delete(deleteThought);
 
 
 module.exports = router;
