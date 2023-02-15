f2b9917b5c7f25702a829a9deca45f9ca593cfe6
gato365 <immanuelwilliams@gato365.com>
2023-02-15 06:55:41 -0800

    Fixed routes for thoughts

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -4,13 +4,17 @@ const {
     createThought,
     getAllThoughts,
     deleteThought,
+    createReaction,
+    deleteReaction
 } = require('../../controllers/thoughtController');
 
 // /api/thoughts
 router.route('/').get(getAllThoughts).post(createThought);
 
 // /api/thoughts/:thoughtId
-router.route('/:thoughtId').delete(deleteThought);
+router.route('/:thoughtId').delete(deleteThought).post(createReaction).delete(deleteReaction);
+
+
 
 
 module.exports = router;
